8558b9d6c73f5e2907aceb843af47461149ccf49
guangya.zhou <chinagyzhou@gmail.com>
2024-07-17 22:42:12 +0800

    19. 删除链表的倒数第 N 个结点

--- 19_removeNthFromEnd/main.cpp
+++ 19_removeNthFromEnd/main.cpp
@@ -51,6 +51,46 @@ public:
 
 
     }
+
+
+    ListNode *removeNthFromEnd3(ListNode *head, int n) {
+        ListNode *dummyNode = new ListNode(0);
+        dummyNode->next = head;
+        ListNode *fast = dummyNode;
+        ListNode *slow = dummyNode;
+        while (n-- && fast != nullptr) {
+            fast = fast->next;
+        }
+        // fast 指针向前移动一步，使得它指向要删除的节点。
+        fast           = fast->next;
+        // 让 fast 指针和 slow 指针同时向前移动，
+        // 直到 fast 指针达到链表末尾。
+        // 这样，当循环结束时，slow 指针指向要删除的节点的前一个节点。
+        while (fast != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        // 修改 slow 指针的 next 指针，将要删除的节点从链表中断开。
+        slow->next = slow->next->next;
+        return dummyNode->next;
+    }
+
+    ListNode *removeNthFromEnd4(ListNode *head, int n) {
+        ListNode *dummyNode = new ListNode(0);
+        dummyNode->next = head;
+        ListNode *fast = dummyNode;
+        ListNode *slow = dummyNode;
+        while (n-- && fast != nullptr) {
+            fast = fast->next;
+        }
+        fast           = fast->next;
+        while (fast != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        return dummyNode->next;
+    }
 };
 
 int main() {
